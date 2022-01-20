1cffd66905786da3184664bb901616adb503221a
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-01-19 22:14:21 -0600

    Functionality start

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./TodoItem.css";
 //check
 //pending
-let checkmark = 'pending'
 
 function TodoItem(props) {
+    
+    const [checkmarkState, setCheckmarkState] = React.useState('pending');
+    const [removeState, setRemoveState] = React.useState(false);
+    
     return (
         <li>
-            <span className={`${checkmark}`}></span>
+            <span 
+                onClick={() => {
+                    setCheckmarkState('check')
+                }} 
+                className={`${checkmarkState}`}
+            ></span>
             <p>{props.text}</p>
             <span className="remove"></span>
         </li>    
