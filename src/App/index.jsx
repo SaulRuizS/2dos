1fd96bc757dc918ac0b9596cb2ed680a0699279f
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-01-26 20:30:34 -0600

    localStorage addition

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -6,28 +6,40 @@ import { TodoItem} from '../Components/TwoDoItem'
 import { CreateTodoButton } from '../Components/CreateTwoDoButton'
 import React from 'react';
 
-const defaultTodos = [
-  { text: '1 TODO',completed: false,},
-  { text: '2 TODO',completed: false,},
-  { text: '3 TODO',completed: false,},
-  { text: '4 TODO',completed: false,},
-  { text: '5 TODO',completed: false,},
-  { text: '6 TODO',completed: false,},
-  { text: '7 TODO',completed: false,},
-  { text: '8 TODO',completed: false,},
-  { text: '9 TODO',completed: false,},
-  { text: '10 TODO',completed: false,},
-  { text: '11 TODO',completed: false,},
-  { text: '12 TODO',completed: false,},
-  { text: '13 TODO',completed: false,},
-  { text: '14 TODO',completed: false,},
-  { text: '15 TODO',completed: false,},
-  { text: 'n TODO',completed: false,},
-]
+// const defaultTodos = [
+//   { text: '1 TODO',completed: false,},
+//   { text: '2 TODO',completed: false,},
+//   { text: '3 TODO',completed: false,},
+//   { text: '4 TODO',completed: false,},
+//   { text: '5 TODO',completed: false,},
+//   { text: '6 TODO',completed: false,},
+//   { text: '7 TODO',completed: false,},
+//   { text: '8 TODO',completed: false,},
+//   { text: '9 TODO',completed: false,},
+//   { text: '10 TODO',completed: false,},
+//   { text: '11 TODO',completed: false,},
+//   { text: '12 TODO',completed: false,},
+//   { text: '13 TODO',completed: false,},
+//   { text: '14 TODO',completed: false,},
+//   { text: '15 TODO',completed: false,},
+//   { text: 'n TODO',completed: false,},
+// ]
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorage2DOs = localStorage.getItem('2DOS_V1');
+  let parsed2DOs;
+
+  if(!localStorage2DOs) {
+    //In case there is no saved data, create a new localStorage
+    localStorage.setItem('2DOS_V1', JSON.stringify( [] ));
+    parsed2DOs = [];
+  } else {
+    parsed2DOs = JSON.parse(localStorage2DOs);
+  }
+
+  const [todos, setTodos] = React.useState(parsed2DOs);
+  // const [todos, setTodos] = React.useState(defaultTodos);
 
   const [searchValue, setSearchValue] = React.useState('');
 
@@ -41,14 +53,20 @@ function App() {
     //If nothing has been writen in the input, show all 2DOs.
     searchedTodos = todos;
   } else {
-    searchedTodos = todos.filter( (todo) => {
+    searchedTodos = todos.filter((todo) => {
       const todoText = todo.text.toLowerCase(); //Avoiding user input issues
       const searchText = searchValue.toLowerCase();
 
       //Filtering every todo that contains the same text introduced by the user.
       return todoText.includes(searchText);
-    } )
+    });
   }
+
+  const save2DOs = (todosToSave) => {
+    const string2DOs = JSON.stringify(todosToSave);
+    localStorage.setItem('2DOS_V1', string2DOs);
+    setTodos(todosToSave);
+  };
 
   const setCompletedState = (text, completedState) => {
     const todoIndex = todos.findIndex( (todo) => todo.text === text );
@@ -57,15 +75,15 @@ function App() {
     // const currentCompleted2DO = completed2DOs[todoIndex];
     // completed2DOs.splice(todoIndex, 1);
     // completed2DOs.push(currentCompleted2DO);
-    setTodos(completed2DOs);
-  }
+    save2DOs(completed2DOs);
+  };
 
   const remove2DO = (text) => {
     const todoIndex = todos.findIndex( (todo) => todo.text === text );
     const new2DOsList = [...todos];
     new2DOsList.splice(todoIndex, 1);
-    setTodos(new2DOsList);
-  }
+    save2DOs(new2DOsList);
+  };
 
   return (
     //The tag below is equal to <React.Fragment>
